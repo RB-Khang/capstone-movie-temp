afb76a259e2d9ce19791a82c3d4bce8e2a4e99ea
Khang <vinhkhang.tl@gmail.com>
2023-09-22 10:55:02 +0700

    first commit

---
+++ src/components/UI/Tabs.tsx
@@ -0,0 +1,19 @@
+import { Tabs as TabsA, TabsProps as TabsPropsA } from 'antd'
+// import React from 'react'
+
+// type Map = {
+//     key?: string
+//     label?: string
+// }
+// type TabsObject=({items,...rest}:TabsPropsA)=>{
+// {
+//     ...rest,
+//     item?
+// }
+// }
+
+
+
+export const Tabs = (props: TabsPropsA) => {
+    return <TabsA {...props} />
+}
--- src/components/templates/RapTemplate.tsx
+++ src/components/templates/RapTemplate.tsx
@@ -1,27 +1,61 @@
+import { Tabs } from "components"
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import { RootState, useAppDispatch } from "store"
-import { HeThongRapThunk } from "store/QuanLyRap/thunk"
+import { CumRapThunk, HeThongRapThunk, ThongTinLichChieuThunk } from "store/QuanLyRap/thunk"
 import styled from "styled-components"
 
 
 const RapTemplate = () => {
     const dispatch = useAppDispatch()
-    const { heThongRap } = useSelector((state: RootState) => state.QuanLyRap)
-    console.log(heThongRap);
+    const { heThongRap, cumRap, ThongTinLichChieu } = useSelector((state: RootState) => state.QuanLyRap)
+    console.log((ThongTinLichChieu));
 
+
+
+    const getCumRap = () => {
+        const cumrap = cumRap?.map((cumrap, index) => {
+            return {
+                key: `${index}`,
+                label: cumrap.tenCumRap,
+                tenCumRap: cumrap.tenCumRap,
+                children: <Tabs tabPosition="left"
+                ></Tabs>,
+            }
+        })
+        return cumrap
+    }
+    const getHeThongRap = () => {
+        const cumrap = getCumRap()
+        const tabsObject = heThongRap?.map((item, index) => {
+            return {
+                key: `${index}`,
+                label: item.tenHeThongRap.toUpperCase(),
+                children: <Tabs
+                    tabPosition="left"
+                    items={cumrap}
+                ></Tabs>,
+                maheThongRap: item.maHeThongRap
+            }
+        })
+        return tabsObject
+    }
+
+    const tabsObject = getHeThongRap()
     useEffect(() => {
         dispatch(HeThongRapThunk())
+        dispatch(CumRapThunk("BHDStar"))
+        dispatch(ThongTinLichChieuThunk())
     }, [dispatch])
     return (
         <div>
             <HeThongRap>
                 <h1 className="p-4 text-4xl font-bold text-center">CÁC HỆ THỐNG RẠP</h1>
-                {
-                    heThongRap?.map(item => {
-                        return <p className="ps-3" key={item.maHeThongRap}>{item.tenHeThongRap}</p>
-                    })
-                }
+                <Tabs tabPosition="left"
+                    items={tabsObject}
+                    onChange={(activeKey) => {
+                        dispatch(CumRapThunk(tabsObject[activeKey].maheThongRap))
+                    }}></Tabs>
             </HeThongRap>
         </div>
     )
@@ -31,9 +65,9 @@ export default RapTemplate
 
 
 const HeThongRap = styled.div`
-height: 400px;
-    width: 50%;
+height: 80vh;
+    width: 80%;
     margin: auto;
-    background-color: #ccc;
-    
+    /* background-color: #e9e9e9;    */
+    overflow: scroll;
 `
